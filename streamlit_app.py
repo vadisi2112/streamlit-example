4918295dad055cdfd5ad49ebd2b0bcbd4a0c10de
vadisi2112 <116917973+vadisi2112@users.noreply.github.com>
2023-05-08 13:02:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ Answer in Markdown:"""
 QA_PROMPT = PromptTemplate(template=template, input_variables=[ "question", "context"])
         
 os.environ['OPENAI_API_KEY']= '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'
-​openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 gpt4 = ChatOpenAI(model="gpt-4", temperature=0, request_timeout=300)
 
@@ -106,8 +106,7 @@ def answer_question(docs, question, method="refine", llm=gpt4):
         {"input_documents": docs, "question": question}, return_only_outputs=True
     )
     return response["output_text"]
-​
-​
+
 def summary(input_text, use_gpt4=True):
     model = 'gpt-4' if use_gpt4 else 'gpt-3.5-turbo'
     chunk_size = 16000 if use_gpt4 else 8000
@@ -117,31 +116,24 @@ def summary(input_text, use_gpt4=True):
 
     chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=True)
     return chain.run(docs)
-​
-​
+
 st.title("Khaleeji GPT")
-​
+
 operation = st.radio(
 "Choose an operation:", [ "Read and answer question", "Read and summarise"]
 )
-​
-if operation == "Read and answer question":
-question = st.text_input("Question:")
-​
-model = st.selectbox("model:", ["gpt-4", "gpt-3.5-turbo"], index=0)
-​
-if st.button("Submit"):
-        with st.spinner("Processing..."):
-            chat_history = []
 
-            answer, chat_history = ask_question(question=question, chat_history=chat_history)
-            if operation == "Read and summarise":
-                f = open(f"1_output.txt", "r")
-                input_text = " ".join(f.readlines()).replace("\n\n", " ")
-                result = summary(input_text)
-            else:
-                result = ask_question(question=question, chat_history=chat_history)
-            st.subheader("Output:")
-            st.write(result)
-​
+if operation == "Read and answer question":
+        chat_history = []
+        question = st.text_input("Question:")
+        
+        result, chat_history = ask_question(question=question, chat_history=chat_history)
+        result appent(
+if operation == "Read and summarise":
+        f = open(f"1_output.txt", "r")
+        input_text = " ".join(f.readlines()).replace("\n\n", " ")
+        result = summary(input_text)
+st.subheader("Output:")
+st.write(result)
+
 
